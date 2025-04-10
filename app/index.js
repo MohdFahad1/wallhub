0c88c1b803a82ab02c32dca6b6c9f8c4b6c15808
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2025-04-10 22:43:12 +0530

    feat: setup nativewind

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import "../global.css";
+
+const Index = () => {
+  return (
+    <View>
+      <Text>Index</Text>
+    </View>
+  );
+};
+
+export default Index;
+
+const styles = StyleSheet.create({});
