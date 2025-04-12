1119e7a7723ad411d99fea2c6b78586171c873fc
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2025-04-12 15:19:43 +0530

    feat: Added category page

--- app/(tabs)/categories.jsx
+++ app/(tabs)/categories.jsx
@@ -1,14 +1,97 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  FlatList,
+  ActivityIndicator,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import ScreenWrapper from "../../components/ScreenWrapper";
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from "react-native-responsive-screen";
+import Animated, { FadeInDown } from "react-native-reanimated";
+
+const AnimatedTouchableOpacity =
+  Animated.createAnimatedComponent(TouchableOpacity);
 
 const Categories = () => {
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchCategories = async () => {
+    try {
+      const response = await fetch(
+        "https://wallhub-server.vercel.app/api/categories"
+      );
+      const json = await response.json();
+      if (json.success) {
+        setCategories(json.categories);
+      } else {
+        setError("Failed to fetch categories");
+      }
+    } catch (err) {
+      setError("An error occurred: " + err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  const renderItem = ({ item, index }) => (
+    <AnimatedTouchableOpacity
+      entering={FadeInDown.delay(index * 200).springify()}
+    >
+      <View
+        className="items-center justify-center mb-5 overflow-hidden rounded-[35px]"
+        style={{
+          width: wp(90),
+          height: hp(18),
+        }}
+      >
+        <Image
+          source={{ uri: item.image }}
+          className="object-cover w-full h-full"
+        />
+        <Text className="absolute text-3xl font-bold text-center text-white">
+          {item.name}
+        </Text>
+      </View>
+    </AnimatedTouchableOpacity>
+  );
+
   return (
-    <View>
-      <Text>Categories Screen</Text>
-    </View>
+    <ScreenWrapper>
+      <Text
+        style={{
+          fontSize: hp(4),
+          marginBottom: 10,
+          textAlign: "center",
+          fontWeight: "bold",
+        }}
+      >
+        Categories
+      </Text>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        {loading ? (
+          <ActivityIndicator size="large" color="#000" />
+        ) : (
+          <FlatList
+            data={categories}
+            renderItem={renderItem}
+            keyExtractor={(item) => item._id}
+            showsVerticalScrollIndicator={false}
+          />
+        )}
+      </View>
+    </ScreenWrapper>
   );
 };
 
 export default Categories;
-
-const styles = StyleSheet.create({});
